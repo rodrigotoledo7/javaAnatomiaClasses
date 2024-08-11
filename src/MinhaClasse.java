b422b7cb14ac343701fd653b04bc4ce58eb69729
Rodrigo Toledo <rodriogtoledo7@gmail.com>
2024-08-10 23:04:17 -0400

    Aula 2.3 - anatomia de classes

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,5 +1,14 @@
 public class MinhaClasse {
     public static void main(String[] args) {
         System.out.println("Olá turma, sejam bem-vindos");
+        String primeiroNome = "Rodrigo";
+        String segundoNome = "Tolêdo";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
     }    
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Resultado do Método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
 }
